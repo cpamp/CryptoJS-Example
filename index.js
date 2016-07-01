77b0e35c9fb0735993e7e885248d07d663d7e917
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-06-30 21:45:00 -0500

    Add: labels

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var salt = CryptoJS.lib.WordArray.random(128/8);
 var password = 'Password2';
 var key = CryptoJS.PBKDF2(password, salt, { keySize: 256/32, iterations: 1000 });
 
-console.log(salt.toString());
-console.log(password);
-console.log(key.toString());
-console.log("Length: " + key.toString().length);
+console.log("Plain Text: " + password);
+console.log("Salt: " + salt.toString());
+console.log("Key: " + key.toString());
+console.log("Salt Length: " + salt.toString().length);
+console.log("Key Length: " + key.toString().length);
